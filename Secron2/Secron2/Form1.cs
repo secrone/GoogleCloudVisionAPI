82766c8b731ff1d8f93a4efc34ce3704703d2bd8
Unknown <arslanfatihfurkan@gmail.com>
2017-04-09 14:43:35 +0300

    windows form ile bir dosyadan alınan resimleri işleyip LABEL DETECTION dönüyor. ve istenilen duruma göre alert veya message box dönüyor.

---
+++ Secron2/Secron2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Secron2
+{
+    public partial class Form1 : Form
+    {
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread th1 = new Thread(new ThreadStart(getResults));
+            th1.Start();
+
+        }
+
+        private void getResults()
+        {
+            
+                var ext = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".png", ".jpg", ".gif" };
+                var dir = @"C:\Users\Toshiba\Desktop\Melike_files";
+                var files =
+                    Directory
+                        .GetFiles(dir, "*.*", SearchOption.AllDirectories)
+                        .Where(f => ext.Contains(Path.GetExtension(f)))
+                        .Select(f => new FileInfo(f))
+                        .ToArray();
+
+                //create service
+                var credentails = Program.CreateCredentials("C:\\Users\\Toshiba\\Desktop\\user_credentials.json");
+                var service = Program.CreateService("Secrone2", credentails);
+
+                //file da ki dosyaları okut
+                foreach (var file in files)
+                {
+                    string f = file.FullName;
+                    Console.WriteLine("Reading " + f + ":");
+
+                    var task = service.AnnotateAsync(file, "LABEL_DETECTION");
+                    var result = task.Result;
+
+                    var keywords = result.LabelAnnotations.Select(s => s.Description).ToArray();
+
+                    foreach(string s in keywords){
+                        if (s == "hair")
+                            MessageBox.Show("Çıktı");
+                            
+                    }
+                    var words = String.Join(", ", keywords);
+                    Console.WriteLine(words);
+
+                    label1.Text = "1.resim için: " + words;
+
+
+                   
+
+                /*    f += ".keywords.txt";
+                    File.WriteAllText(f, words);*/
+                }
+            
+        }
+
+    }
+}
---
+++ Secron2/Secron2/Program.cs
@@ -0,0 +1,103 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Http;
+using Google.Apis.Services;
+using Google.Apis.Vision.v1;
+using Google.Apis.Vision.v1.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Secron2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Console.WriteLine("sdfsdfsd");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+          
+
+        }
+
+        public static GoogleCredential CreateCredentials(string path)
+        {
+            GoogleCredential credential;
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                var c = GoogleCredential.FromStream(stream);
+                credential = c.CreateScoped(VisionService.Scope.CloudPlatform);
+            }
+
+            return credential;
+        }
+
+        public static VisionService CreateService(
+    string applicationName,
+    IConfigurableHttpClientInitializer credentials)
+        {
+            var service = new VisionService(new BaseClientService.Initializer()
+            {
+                ApplicationName = applicationName,
+                HttpClientInitializer = credentials
+            });
+
+            return service;
+        }
+
+        private static AnnotateImageRequest CreateAnnotationImageRequest(
+    string path,
+    string[] featureTypes)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Not found.", path);
+            }
+
+            var request = new AnnotateImageRequest();
+            request.Image = new Image();
+
+            var bytes = File.ReadAllBytes(path);
+            request.Image.Content = Convert.ToBase64String(bytes);
+
+            request.Features = new List<Feature>();
+
+            foreach (var featureType in featureTypes)
+            {
+                request.Features.Add(new Feature() { Type = featureType });
+            }
+
+            return request;
+        }
+
+        public static async Task<AnnotateImageResponse> AnnotateAsync(
+    this VisionService service,
+    FileInfo file,
+    params string[] features)
+        {
+            var request = new BatchAnnotateImagesRequest();
+            request.Requests = new List<AnnotateImageRequest>();
+            request.Requests.Add(CreateAnnotationImageRequest(file.FullName, features));
+
+            var result = await service.Images.Annotate(request).ExecuteAsync();
+
+            if (result.Responses.Count > 0)
+            {
+                return result.Responses[0];
+            }
+
+            return null;
+        }
+    }
+}
